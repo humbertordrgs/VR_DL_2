5b852bf5f69e21cc03034c809059d0ab6af35243
unknown <davidrojas0791@gmail.com>
2021-05-21 18:58:23 -0400

    advances

--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,24 @@
 import os
 from PIL import Image
 
-def load_images(image_gallery_path, mapping_file_path):
+def get_class_map(mapping_file_path):
   class_map = {}
   with open(mapping_file_path,'r') as f1:
     content = f1.read().splitlines()
     for line in content:
       parts = line.split(sep="\t")
-      class_map[parts[0].replace('-','_')] = parts[1]  
+      class_map[parts[1]] = parts[0].replace('-','_')
+  return class_map
+
+def load_images(image_gallery_path, mapping_file_path): 
   images = {}
   if (os.path.isdir(image_gallery_path)):
     for class_name in os.listdir(image_gallery_path):
-      class_idx = class_map[class_name]
-      if (class_idx not in images):
-        images[class_idx] = []
+      if (class_name not in images):
+        images[class_name] = []
       for file_name in os.listdir(image_gallery_path + '/' + class_name):
-        images[class_idx].append(file_name)
+        images[class_name].append(file_name)
   return images
-
 
 def get_processed_img(img_path, preprocess_pipeline):
   image = Image.open(img_path)
