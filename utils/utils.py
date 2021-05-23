e93859982b82553558938643ee56ff139e47aed5
unknown <davidrojas0791@gmail.com>
2021-05-23 00:08:32 -0400

    added normalization to image transformation pipeline

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,7 @@ preprocess_pipeline = transforms.Compose([
   transforms.Resize(256),
   transforms.CenterCrop(224),
   transforms.ToTensor(),
+  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 def get_class_map(mapping_file_path):
