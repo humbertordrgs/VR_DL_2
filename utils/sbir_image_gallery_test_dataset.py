fd4959dfba5ac3d6b7c417929979ddb5c6f402d4
unknown <davidrojas0791@gmail.com>
2021-05-23 18:29:59 -0400

    advances

--- utils/sbir_image_gallery_test_dataset.py
+++ utils/sbir_image_gallery_test_dataset.py
@@ -2,25 +2,20 @@
 from torch.utils.data import Dataset
 
 # External tool (built by us) to format the dataset structure
-from utils import load_images, get_class_map, get_processed_img, execution_time
+from utils import load_images, get_processed_img, execution_time
 
-class SketchBasedImageRetrievalTestDataset(Dataset):
+class SBIRImageGalleryTestDataset(Dataset):
   
   @execution_time
-  def __init__(self, sketch_folder_path, image_gallery_folder_path, mapping_file_path):
+  def __init__(self, image_gallery_folder_path):
       
     self.images = []
-
-    class_map = get_class_map(sketch_folder_path + "/" + mapping_file_path)
-    # print(class_map)
     structured_images = load_images(image_gallery_folder_path)
     for key in structured_images:
       for images in structured_images[key]:  
         image_path = f"{image_gallery_folder_path}/{key}/{images}"
         self.images.append(get_processed_img(image_path))
 
-          
-          
   def __len__(self):
     return len(self.images)
 
---
+++ utils/sbir_sketch_test_dataset.py
@@ -0,0 +1,24 @@
+import os
+
+# PyTorch and numpy related
+from torch.utils.data import Dataset
+
+# External tool (built by us) to format the dataset structure
+from utils import get_processed_img, execution_time
+
+class SBIRSketchTestDataset(Dataset):
+  
+  @execution_time
+  def __init__(self, sketch_folder_path):
+      
+    self.sketches = []
+
+    for sketch_path in os.listdir(sketch_folder_path): 
+        sketch_path = f"{sketch_folder_path}/{sketch_path}"
+        self.sketches.append(get_processed_img(sketch_path))
+
+  def __len__(self):
+    return len(self.sketches)
+
+  def __getitem__(self, idx):
+    return self.sketches[idx]
